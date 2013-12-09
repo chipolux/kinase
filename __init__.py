e5c9de9a5e3ef3643c8e75cf715ea7c0148d2ee1
NaKyle Wright <nzwrigh@wal-mart.com>
2013-12-09 10:02:42 -0600

    A to AN, big difference!

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ __all__ = ['SNMPHelper']
 
 __version__ = '0.1'
 
-__doc__ = ("kinase = kinase is not a snmp engine\n"
+__doc__ = ("kinase = kinase is not an snmp engine\n"
            "Kinase, an enzyme that performs transfers between molecules and substrates.\n"
            "This is a helper module to work with basic SNMP requests.\n"
            "It's meant as a replacement for larger and more full featured frameworks like"
